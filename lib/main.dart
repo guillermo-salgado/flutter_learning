15970064e82d48b3652964753bf1ae9b85b104be
Guillermo Crescencio <guillermo.crescencio@laureate.net>
2019-01-10 14:29:02 -0600

    Shopping list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/shopping-list .dart';
+import 'package:flutter_learning/shopping-list-item.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Flutter Tutorial',
-    home: TutorialHome(),
+    title: 'Shopping App',
+    home: ShoppingList(
+      products: <Product>[
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Chocolate chips'),
+      ],
+    ),
   ));
-}
-
-class TutorialHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Scaffold is a layout for the major Material Components.
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      // body is the majority of the screen.
-      body: Center(
-        child: Counter(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('MyButton was pressed!');
-        },
-      ),
-    );
-  }
-}
-
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({this.count});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: onPressed,
-      child: Text('Increment'),
-    );
-  }
-}
-
-class Counter extends StatefulWidget {
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      ++_counter;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      CounterIncrementor(onPressed: _increment),
-      CounterDisplay(count: _counter),
-    ]);
-  }
 }
